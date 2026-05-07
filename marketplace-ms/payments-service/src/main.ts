ccacfe63f11bcfaa1ef4371fbfd89bb19338b029
Giovanni Henrique Gazin Pasa <giovannihgp@hotmail.com>
2026-05-07 15:53:08 -0300

    feat: init structure payments-service

---
+++ marketplace-ms/payments-service/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3004;
+  await app.listen(process.env.PORT || port);
+
+  console.log(`💳 Payments Service running on port ${port}`);
+}
+bootstrap();
